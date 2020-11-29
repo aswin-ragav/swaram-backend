eaf85b03eaa776ead7cdb88b387dc5c082e818f2
AR <aswin.achu@outlook.com>
2020-11-29 23:09:54 +0530

    Added Spring Cloud Config support for discovery-server

--- discovery-server/src/main/java/com/server/discovery/DiscoveryServer.java
+++ discovery-server/src/main/java/com/server/discovery/DiscoveryServer.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class DiscoveryServer {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "discovery-server");
 		SpringApplication.run(DiscoveryServer.class, args);
 	}
 
